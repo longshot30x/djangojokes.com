194dc1acccf47beb98118fcefddcf9f4e67733e9
joefacio <66976425+joefacio@users.noreply.github.com>
2025-04-08 14:07:22 -0700

    Jokes App Update

--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
 
 
 class JokeDetailView(DetailView):
@@ -9,3 +13,7 @@ class JokeDetailView(DetailView):
 class JokeListView(ListView):
     model = Joke
     
+    
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
