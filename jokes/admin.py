25f089838fab52b46ef98a9cdf9cdd4934f65a13
joefacio <66976425+joefacio@users.noreply.github.com>
2025-05-05 18:45:34 -0700

    categories

--- jokes/admin.py
+++ jokes/admin.py
@@ -11,3 +11,12 @@ class CategoryAdmin(admin.ModelAdmin):
             return ('slug', 'created', 'updated')
         return ()
 
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke  
+    list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
