3ec75f7f4be7bd6d0bc9ee8bd5a137829ba9e4b8
joefacio <66976425+joefacio@users.noreply.github.com>
2025-05-06 20:40:44 -0700

    tags

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Joke
+from .models import Category, Joke, Tag 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -15,6 +15,16 @@ class CategoryAdmin(admin.ModelAdmin):
 class JokeAdmin(admin.ModelAdmin):
     model = Joke  
     list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
--- jokes/forms.py
+++ jokes/forms.py
@@ -4,7 +4,7 @@ from .models import Joke
 class JokeForm(ModelForm):
     class Meta:
         model = Joke
-        fields = ['question', 'answer', 'category']
+        fields = ['question', 'answer', 'category', 'tags']
         widgets = {
             'question': Textarea(
                 attrs={'cols': 80, 'rows': 3, 'autofocus': True}
@@ -14,5 +14,8 @@ class JokeForm(ModelForm):
             )
         }
         help_texts = {
-            'question': 'No dirty jokes please.'
+            'question': 'No dirty jokes please.',
+            'tags': 'Use Ctrl-click to select multiple tags.'
         }
+
+
