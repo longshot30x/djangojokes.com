2b8a1ccd78ed157eb2be886b26713260b7d43171
joefacio <66976425+joefacio@users.noreply.github.com>
2025-05-14 00:12:50 -0700

    fixing

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Category, Joke, Tag 
+
+from .models import Category, Joke, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    model = Category 
+    model = Category
     list_display = ['category', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
@@ -11,15 +12,18 @@ class CategoryAdmin(admin.ModelAdmin):
             return ('slug', 'created', 'updated')
         return ()
 
+
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
-    model = Joke  
+    model = Joke
     list_display = ['question', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
+
         return ()
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
