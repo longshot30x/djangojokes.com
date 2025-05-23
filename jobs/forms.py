25d505236c0b8e71e5e180eb17a4ad37885ced94
joefacio <66976425+joefacio@users.noreply.github.com>
2025-05-22 22:55:29 -0700

    crispy-forms

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,45 +1,53 @@
 from datetime import datetime
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
 
-
-
+def validate_future_date(value):
+    if value < datetime.now().date():
+        raise ValidationError(
+            message=f'{value} is in the past.', code='past_date'
+        )
+    
 
 class JobApplicationForm(forms.Form):
     EMPLOYMENT_TYPES = (
         (None, '--Please choose--'),
-        ('ft,' 'Full-time'),
-        ('pt,' 'Part-time'),
+        ('ft', 'Full-time'),
+        ('pt', 'Part-time'),
         ('contract', 'Contract work')
     )
-    
+
     DAYS = (
         (1, 'MON'),
         (2, 'TUE'),
         (3, 'WED'),
         (4, 'THU'),
         (5, 'FRI')
-        
     )
     
     YEARS = range(datetime.now().year, datetime.now().year+2)
 
     first_name = forms.CharField(
-        widget=forms.TextInput(attrs={'autofocus': True})
+       widget=forms.TextInput(attrs={'autofocus': True})
     )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(
+    website = forms.CharField(
         required=False,
-        widget=forms.URLInput(
+        widget=forms.TextInput(
             attrs={'placeholder':'https://www.example.com', 'size':'50'}
-        )
+        ),
+        validators=[URLValidator(schemes=['http', 'https'])]
     )
     employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
     start_date = forms.DateField(
         help_text='The earliest date you can start working.',
         widget=forms.SelectDateWidget(
             years=YEARS,
-        )
+        ),
+        validators=[validate_future_date],
+        error_messages = {'past_date': 'Please enter a future date.'}
     )
     available_days = forms.TypedMultipleChoiceField(
         choices=DAYS,
@@ -60,31 +68,3 @@ class JobApplicationForm(forms.Form):
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
-
-    class Meta:
-        #model = Applicant
-        fields = (
-            'first_name', 'last_name', 'email', 'website', 'employment_type',
-            'start_date', 'available_days', 'desired_hourly_wage',
-            'cover_letter', 'confirmation', 'job')
-        widgets = {
-            'first_name': forms.TextInput(attrs={'autofocus': True}),
-            'website': forms.TextInput(
-                attrs = {'placeholder':'https://www.example.com'}
-            ),
-            'start_date': forms.SelectDateWidget(
-                attrs = {
-                    'style': 'width: 21%; display: inline-block; margin: 0 1%'
-                },
-                years = range(datetime.now().year, datetime.now().year+2)
-            ),
-            'desired_hourly_wage': forms.NumberInput(
-                attrs = {'min':'10.00', 'max':'100.00', 'step':'.25'}
-            ),
-            'cover_letter': forms.Textarea(attrs={'cols': '100', 'rows': '5'})
-        }
-        error_messages = {
-            'start_date': {
-                'past_date': 'Please enter a future date.'
-            }
-        }
